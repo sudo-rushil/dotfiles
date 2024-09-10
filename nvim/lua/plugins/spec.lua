d2fe55b20fc0635b1562976e740118dcfa8e4be4
Rushil Mallarapu <rushil.mallarapu@gmail.com>
2024-09-10 16:52:54 -0400

    updated aerospace

--- nvim/lua/plugins/spec.lua
+++ nvim/lua/plugins/spec.lua
@@ -1,4 +1,14 @@
 return {
     -- colorscheme
     { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.cmd [[Lazy load markdown-preview.nvim]]
+            vim.fn["mkdp#util#install"]()
+        end,
+    }
 }
