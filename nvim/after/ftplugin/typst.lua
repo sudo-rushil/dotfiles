236bc15f17c94ca7fa45e992e852ae2117809e39
Rushil Mallarapu <rushil.mallarapu@gmail.com>
2025-04-01 20:18:59 -0400

    spelling changes

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_user_command("SetMain", function()
 	local filepath = vim.api.nvim_buf_get_name(0)
+	-- local client = vim.lsp.get_client_by_id(event.data.client_id)
 	if filepath:match("%.typ$") then
 		vim.lsp.buf.execute_command({
 			command = "tinymist.pinMain",
