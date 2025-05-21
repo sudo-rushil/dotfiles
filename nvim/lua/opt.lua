3bcba454fe73c4322087088e781390da7f90537c
Rushil Mallarapu <rushil.mallarapu@pm.me>
2025-05-21 11:57:24 -0400

    switched to oil for file browser

--- nvim/lua/opt.lua
+++ nvim/lua/opt.lua
@@ -41,7 +41,7 @@ vim.opt.inccommand = "split"
 -- keep 5 lines above/below cursor
 vim.opt.scrolloff = 5
 
-vim.keymap.set("n", "<leader>sp", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>sp", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", ":", ";")
 vim.keymap.set("n", ";", ":")
 
