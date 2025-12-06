a8ad42ab2fff64131387e7987e4bc3e33f093d7c
Rushil Mallarapu <rushil.mallarapu@pm.me>
2025-12-05 23:45:39 -0500

    add js/ts support in javascript

--- doom/init.el
+++ doom/init.el
@@ -144,7 +144,7 @@
        ;;json              ; At least it ain't XML
        janet             ; Fun fact: Janet is me!
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -175,6 +175,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
+       typescript
        ;;web               ; the tubes
        ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
