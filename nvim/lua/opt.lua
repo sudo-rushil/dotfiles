06fc179b5325e2a8965c2a3e6ef288d6a01b0fd7
Rushil Mallarapu <rushil.mallarapu@pm.me>
2026-02-01 14:03:33 +0800

    feat: add code grab for nvim

--- nvim/lua/opt.lua
+++ nvim/lua/opt.lua
@@ -69,4 +69,28 @@ vim.filetype.add({
 	},
 })
 
+-- Copy selection with file context (filename:line_range + code)
+vim.keymap.set("v", "<leader>y", function()
+	local start_line = vim.fn.line("v")
+	local end_line = vim.fn.line(".")
+	if start_line > end_line then
+		start_line, end_line = end_line, start_line
+	end
+
+	local filename = vim.fn.expand("%:.") -- relative path
+	local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+	local code = table.concat(lines, "\n")
+
+	local line_ref
+	if start_line == end_line then
+		line_ref = tostring(start_line)
+	else
+		line_ref = start_line .. "-" .. end_line
+	end
+
+	local result = filename .. ":" .. line_ref .. "\n" .. code
+	vim.fn.setreg("+", result)
+	vim.notify("Copied: " .. filename .. ":" .. line_ref, vim.log.levels.INFO)
+end, { desc = "Yank selection with context" })
+
 -- vim: ts=2 sts=2 sw=2 et
