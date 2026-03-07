f5f39bb72bfc72005cef895a0bcde29e214c77bb
Rushil Mallarapu <rushil.mallarapu@pm.me>
2026-03-08 01:49:41 +0800

    feat: prevent autocompletion for single quote

--- nvim/lua/plugins/spec.lua
+++ nvim/lua/plugins/spec.lua
@@ -464,7 +464,7 @@ return {
 					["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
 					--['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-					--["'"] = false, -- { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+					["'"] = false, -- { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
 					--["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
 				},
 			})
