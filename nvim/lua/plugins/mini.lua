0965a51759cf2632df479ce41059d540003e8a42
Rushil Mallarapu <rushil.mallarapu@pm.me>
2026-03-08 23:57:37 +0800

    fix: add mini diff to visualize active changes

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -18,7 +18,8 @@ require("mini.ai").setup({ n_lines = 500 })
 require("mini.surround").setup()
 
 require("mini.indentscope").setup()
-require("mini.trailspace").setup()
+
+require("mini.diff").setup()
 
 require("mini.pairs").setup({
 	mappings = {
