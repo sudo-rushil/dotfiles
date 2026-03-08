8744a269568542073ad793c465ac3a12843c1171
Rushil Mallarapu <rushil.mallarapu@pm.me>
2026-03-08 23:46:57 +0800

    feat: add lazydev.nvim and luvit for lua lsp support
    Maybe remove or defer loading these later?

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,25 +7,3 @@ vim.g.have_nerd_font = true
 require("config")
 require("plugins")
 require("config.lsp")
--- require("opt")
-
--- load lazy.nvim
--- require("config.lazy")
-
--- require'lspconfig'.clangd.setup{}
--- vim: ts=2 sts=2 sw=2 et
-
---biome
---clangd
---rust-analyzer
---lua_ls
---stylua
---tailwindcss
---tinymist (not needed now)
---typescript
-
--- formatters
--- stylua
--- clang-format
--- rustfmt
--- biome
