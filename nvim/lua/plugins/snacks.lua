7fa9685bae7eff0b3cf0245052f4eac7aeedff57
Rushil Mallarapu <rushil.mallarapu@pm.me>
2026-03-08 21:19:35 +0800

    feat: add further snacks picker keymaps

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -10,10 +10,12 @@ snacks.setup({
   bigfile = {enabled = true},
   dashboard = {enabled = false},
   dim = {enabled = true},
+  explorer = {enabled = true},
   image = {enabled = true},
   indent = {enabled = true},
   input = {enabled = true},
   layout = {enabled = true},
+  lazygit = {enabled = true},
   notifier = {enabled = true},
   scope = {enabled = true},
   scroll = {enabled = true},
@@ -104,8 +106,28 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 local keymaps = {
+  -- Search
+  { "<leader><leader>", function() snacks.picker.buffers() end, desc = "Search through buffers" },
   { "<leader>sf", function() snacks.picker.files() end, desc = "Find files" },
-  { "<leader>sp", function() snacks.picker.projects() end, desc = "Projects" }
+  { "<leader>sg", function() snacks.picker.grep() end, desc = "Grep" },
+  { "<leader>s/", function() snacks.picker.grep_buffers() end, desc = "Grep open buffers" },
+  { "<leader>sn", function() snacks.picker.files({ cwd = vim.fn.stdpath("config")}) end, desc = "Find editor config files" },
+  { "<leader>sp", function() snacks.picker.projects() end, desc = "Projects" },
+  -- Explorer
+  { "<leader>se", function() snacks.explorer() end, desc = "File explorer" },
+  -- Git
+  { "<leader>ts", function() snacks.picker.git_status() end, desc = "Git status" },
+  -- LSP
+  { "<leader>gd", function() snacks.picker.lsp_definitions() end, desc = "Goto definition" },
+  { "<leader>gD", function() snacks.picker.lsp_type_definitions() end, desc = "Goto type definition" },
+  -- Buffers
+  { "<leader>bd", function() snacks.bufdelete() end, desc = "Delete buffer" },
+  -- Other
+  { "<leader>uz", function() snacks.zen() end, desc = "Toggle zen mode" },
+  { "<leader>uw", function() snacks.toggle.option("wrap", {name = "Wrap"}) end, desc = "Toggle wrap" },
+  { "<leader>us", function() snacks.toggle.option("spell", {name = "Spelling"}) end, desc = "Toggle spellcheck" },
+  { "<leader>un", function() snacks.notifier.hide() end, desc = "Dismiss all notifications" },
+  { "<leader>gg", function() snacks.lazygit() end, desc = "Lazygit" },
 }
 
 for _, map in ipairs(keymaps) do
