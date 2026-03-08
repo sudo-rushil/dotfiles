619bfc3d67a10e81ac7510195c88e146236b974d
Rushil Mallarapu <rushil.mallarapu@pm.me>
2026-03-08 14:00:38 +0800

    feat: set up autocommands

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,64 @@
+local augroup = function(name)
+	return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+	group = augroup("checktime"),
+	callback = function()
+		if vim.o.buftype ~= "nofile" then
+			vim.cmd("checktime")
+		end
+	end,
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup("highlight_yank"),
+	callback = function()
+		(vim.hl or vim.highlight).on_yank()
+	end,
+})
+
+-- Close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("close_with_q"),
+	pattern = {
+		"checkhealth",
+		"grug-far",
+		"help",
+		"lspinfo",
+		"notify",
+		"qf",
+		"startuptime",
+		"oil",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.schedule(function()
+			vim.keymap.set("n", "q", function()
+				vim.cmd("close")
+				pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+			end, {
+				buffer = event.buf,
+				silent = true,
+				desc = "Quit buffer",
+			})
+		end)
+	end,
+})
+
+-- Spellcheck in text files
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("wrap_spell"),
+	pattern = {
+		"text",
+		"typst",
+		"gitcommit",
+		"jjdescription",
+		"markdown",
+	},
+	callback = function()
+		vim.opt_local.spell = true
+	end,
+})
